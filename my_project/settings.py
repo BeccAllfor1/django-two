33c45e8a58939f4e0f0c9d45372e942fec60c8ff
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-11 09:53:36 +0000

    updated files and settings for deployment and new project

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9a!4u5u$y&yukxsr-x3g6ana1_&z^-^f2(9pz^=@hn-__73-z2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-beccallfor1-djangotwo-gujfwybcdxl.ws.codeinstitute-ide.net']
 
